6055a932ac94f6afa7cd4873398c6be5928a3659
Andy Erickson <andy_erickson@digikey.com>
2019-01-28 14:38:07 -0600

    enabled cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
